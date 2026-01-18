8345ba9c9115ce65943ac2789ad19fdf05b60e83
lmangall-42lab <l.mangallon@gmail.com>
2026-01-18 14:28:43 +0100

    fix: remove baseURL config from auth client

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,8 +2,6 @@
 
 import { createAuthClient } from 'better-auth/react';
 
-export const authClient = createAuthClient({
-  baseURL: '',
-});
+export const authClient = createAuthClient();
 
 export const { signIn, signUp, signOut, useSession } = authClient;
